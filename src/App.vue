4a01c2e9ec6c77e4b54c7b4fc978322a5bf3b006
nojaja <free.riccia@gmail.com>
2020-03-16 00:25:15 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="columns">
+          <div class="column">
+            <h2 class="title">Vertical Panes</h2>
+          </div>
+          <div class="column is-narrow">
+            <a href="https://github.com/yansern/vue-multipane/blob/master/demo/src/VerticalPanes.vue" class="button is-warning">View code &raquo;</a>
+          </div>
+        </div>
+        <VerticalPanes></VerticalPanes>
+      </div>
+    </section>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="columns">
+          <div class="column">
+            <h2 class="title">Horizontal Panes</h2>
+          </div>
+          <div class="column is-narrow">
+            <a href="https://github.com/yansern/vue-multipane/blob/master/demo/src/HorizontalPanes.vue" class="button is-warning">View code &raquo;</a>
+          </div>
+        </div>
+        <HorizontalPanes></HorizontalPanes>
+      </div>
+    </section>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="columns">
+          <div class="column">
+            <h2 class="title">Custom Resizer</h2>
+          </div>
+          <div class="column is-narrow">
+            <a href="https://github.com/yansern/vue-multipane/blob/master/demo/src/CustomResizer.vue" class="button is-warning">View code &raquo;</a>
+          </div>
+        </div>
+        <CustomResizer></CustomResizer>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import VerticalPanes from './VerticalPanes.vue'
+import HorizontalPanes from './HorizontalPanes.vue'
+import CustomResizer from './CustomResizer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    VerticalPanes,
+    HorizontalPanes,
+    CustomResizer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
